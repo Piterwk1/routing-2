005e46dde2d9fe784a5dd7ddfa31f581e7af4348
Piotr Kaminski <piterwk1@.wp.pl>
2021-01-03 11:33:27 +0100

    initial commit

---
+++ src/layouts/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+ 
+  render() { 
+    return ( 
+      <div>
+        działa
+      </div>
+     );
+  }
+}
+ 
+export default App;
+
+
