e1aaba01daaa0fb6b1ae6b762f1e06ecbc951397
Piotr Kaminski <piterwk1@.wp.pl>
2021-01-05 18:59:08 +0100

    add contactPage.js and ContactPage.css

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import '../styles/ContactPage.css';
+import { Prompt} from 'react-router-dom'
 
-const ContactPage = () => {
+
+class ContactPage extends React.Component {
+
+    state = {
+        value: ''
+    }
+
+handleSubmit = (e) => {
+     e.preventDefault()
+     this.setState({
+         value: ''
+     })
+}
+
+handleChange = (e) => {
+this.setState({
+    value: e.target.value
+})
+}
+
+    render() {
     return ( 
-        <div>Contact</div>
+        <div className='contact'>
+            <form onSubmit={this.handleSubmit}>
+                <h3>Napisz do nas</h3>
+                <textarea value={this.state.value} onChange={this.handleChange} placeholder='wpisz wiadomosc....'></textarea>
+                <button>Wyslij</button>
+            </form>
+            <Prompt
+            when={this.state.value}
+            message='masz niewypelniony formularz. Czy napewno chcesz opuscic ta strone'
+            />
+        </div>
      );
+    }
 }
  
 export default ContactPage;
